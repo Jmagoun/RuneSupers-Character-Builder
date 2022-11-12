cb4e301cb0e32835534cad7b2ed94ce49c7a01eb
Jmagoun <jmagoun242@gmail.com>
2022-11-12 14:33:44 -0500

    upload files
    upload base files/json test files

---
+++ StatSkillCalc.js
@@ -0,0 +1,20 @@
+function StatSkillCalc()
+{
+  
+  //stat variables (letters so it's easier to parse the equations because of eval)
+    var M;    //Muscle
+    var S;    //Skill
+    var R;    //Reflex
+    var T;    //Toughness
+    var W;    //Wit
+    var P;    //Power
+    var C;    //Charisma
+    
+    
+
+  
+    //doing prompts for now to set up math and logic  
+
+
+
+}7
